d6ece8d1204c3d9d68b9a001229fe9de5706e5ca
Caio <caioh.maiab@gmail.com>
2025-11-06 23:15:55 -0300

    ajustando import errado

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -1,6 +1,5 @@
 // Barrel export for animations
 export { DotsBackground } from './DotsBackground';
-export { TypeWriter } from './TypeWriter';
 export { ScrambleText } from './ScrambleText';
 export { ScrollIndicator } from './ScrollIndicator';
 export { GlitchEffect } from './GlitchEffect';
